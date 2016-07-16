f70815734b193f86228ebf0b8f0b2450275f07b9
zhanghui <zhanghui@ablecloud.cn>
2016-07-16 21:15:54 +0800

    update rest

--- rest/rest.go
+++ rest/rest.go
@@ -1,7 +1,6 @@
 package rest
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -11,21 +10,14 @@ import (
 	"golang.org/x/net/context"
 )
 
-type Encoder interface {
-	Encode(io.Reader, interface{}) error
-}
-
-type Decoder interface {
-	Decode(io.Reader, interface{}) error
-}
-
 type Codec interface {
-	Encoder
-	Decoder
+	Decode(io.Reader, interface{}) error
+	Encode(io.Writer, interface{}) error
 }
 
 type Rest struct {
-	mux *pat.PatternServeMux
+	mux    *pat.PatternServeMux
+	codecs map[string]Codec
 }
 
 func New() *Rest {
@@ -54,22 +46,32 @@ func (rest *Rest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rest *Rest) serve(m *method, w http.ResponseWriter, r *http.Request) {
+	contentType := getContentType(r.Header)
+
+	//lookup codec
+	codec, ok := rest.lookupCodec(contentType)
+	if !ok {
+		setErr(w, http.StatusBadRequest, fmt.Errorf("unsupport Content-Type: %s", contentType))
+		return
+	}
+
+	var err error
 	argv := m.NewArg()
 	replyv := m.NewReply()
 
-	var err error
+	//Decode
 	if !m.ArgIsNullInterface() {
 		if r.Body == nil {
 			setErr(w, http.StatusBadRequest, errors.New("body is nil"))
 			return
 		}
-		dec := json.NewDecoder(r.Body)
-		if err = dec.Decode(argv.Interface()); err != nil {
+		if err = codec.Decode(r.Body, argv.Interface()); err != nil {
 			setErr(w, http.StatusBadRequest, err)
 			return
 		}
 	}
 
+	//Call
 	values := r.URL.Query()
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "values", values)
@@ -79,13 +81,36 @@ func (rest *Rest) serve(m *method, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	setContentType(w.Header(), contentType)
+
+	//Encode
 	if !m.ReplyIsNullInterface() {
-		enc := json.NewEncoder(w)
-		if err = enc.Encode(replyv.Interface()); err != nil {
+		if err = codec.Encode(w, replyv.Interface()); err != nil {
 			setErr(w, http.StatusInternalServerError, err)
 			return
 		}
 	}
+}
+
+func (rest *Rest) lookupCodec(name string) (Codec, bool) {
+	c, ok := rest.codecs[name]
+	return c, ok
+}
+
+func getSeq(h http.Header) string {
+	return h.Get("X-Sequence")
+}
+
+func setSeq(h http.Header, v string) {
+	h.Set("X-Sequence", v)
+}
+
+func getContentType(h http.Header) string {
+	return h.Get("Content-Type")
+}
+
+func setContentType(h http.Header, v string) {
+	h.Set("Content-Type", v)
 }
 
 func setErr(w http.ResponseWriter, status int, err error) {
