ecb3e7d1eb3cecb6de8a7b827ed32af2c02b45b4
zhanghui <zhanghui@ablecloud.cn>
2017-06-01 17:41:03 +0800

    add tags pkg

---
+++ tags/tags.go
@@ -0,0 +1,31 @@
+package tags
+
+import "strings"
+
+type TagOptions string
+
+func ParseTag(tag string) (string, TagOptions) {
+	if idx := strings.Index(tag, ","); idx != -1 {
+		return tag[:idx], TagOptions(tag[idx+1:])
+	}
+	return tag, TagOptions("")
+}
+
+func (o TagOptions) Contains(optionName string) bool {
+	if len(o) == 0 {
+		return false
+	}
+	s := string(o)
+	for s != "" {
+		var next string
+		i := strings.Index(s, ",")
+		if i >= 0 {
+			s, next = s[:i], s[i+1:]
+		}
+		if s == optionName {
+			return true
+		}
+		s = next
+	}
+	return false
+}
