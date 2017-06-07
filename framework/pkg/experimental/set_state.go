eacd7ad33f7d94409d14baabfd15d13678c9940e
zhanghui <zhanghui@ablecloud.cn>
2017-06-07 18:47:56 +0800

    update experimental

--- framework/pkg/experimental/set_state.go
+++ framework/pkg/experimental/set_state.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"runtime"
 
 	"github.com/ironzhang/matrix/errs"
 )
@@ -92,21 +93,179 @@ func (e setError) Error() string {
 	return fmt.Sprintf("can not set value from %s[%s] to %s[%s]", e.src.Kind(), e.src, e.dst.Kind(), e.dst)
 }
 
+func SetValue(x, y interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if _, ok := r.(runtime.Error); ok {
+				panic(r)
+			}
+			if e, ok := r.(error); ok {
+				err = e
+				return
+			}
+			panic(r)
+		}
+	}()
+
+	setState{}.set(reflect.ValueOf(x), reflect.ValueOf(y))
+	return
+}
+
 type setState struct {
 }
 
 // x = y
-func (s *setState) set(x, y reflect.Value) {
+func (s setState) set(x, y reflect.Value) {
+	y = indirect(y)
+	switch y.Kind() {
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64:
+		indirect(x).Set(y)
+
+	case reflect.String:
+		s.setString(x, y)
+
+	case reflect.Array, reflect.Slice:
+		s.setArray(x, y)
+
+	case reflect.Map:
+		s.setMap(x, y)
+
+	case reflect.Struct:
+		s.setStruct(x, y)
+
+	default:
+		s.panic("set", setError{src: y.Type(), dst: x.Type()})
+	}
 }
 
-func (s *setState) setValue(x, y reflect.Value) {
+func (s setState) setString(x, y reflect.Value) {
+	u, ut, v := indirectUnmarshaler(x)
+	if u != nil {
+		b := []byte(`"` + y.String() + `"`)
+		if err := u.UnmarshalJSON(b); err != nil {
+			s.panic("setString", setError{src: y.Type(), dst: x.Type(), err: err})
+		}
+		return
+	}
+	if ut != nil {
+		b := []byte(y.String())
+		if err := ut.UnmarshalText(b); err != nil {
+			s.panic("setString", setError{src: y.Type(), dst: x.Type(), err: err})
+		}
+		return
+	}
+	v.Set(y)
 }
 
-func (s *setState) setString(x, y reflect.Value) {
+func (s setState) setArray(x, y reflect.Value) {
+	x = indirect(x)
+	switch x.Kind() {
+	case reflect.Interface:
+		if x.NumMethod() != 0 {
+			s.panic("setArray", setError{src: y.Type(), dst: x.Type()})
+		}
+		x.Set(y)
+
+	case reflect.Array:
+		for i := 0; i < x.Len(); i++ {
+			v := x.Index(i)
+			v.Set(reflect.Zero(v.Type()))
+		}
+		for i := 0; i < x.Len() && i < y.Len(); i++ {
+			s.set(x.Index(i), y.Index(i))
+		}
+
+	case reflect.Slice:
+		ns := reflect.MakeSlice(x.Type(), y.Len(), y.Len())
+		for i := 0; i < y.Len(); i++ {
+			s.set(ns.Index(i), y.Index(i))
+		}
+		x.Set(ns)
+
+	default:
+		s.panic("setArray", setError{src: y.Type(), dst: x.Type()})
+	}
 }
 
-func (s *setState) setMap(x, y reflect.Value) {
+func (s setState) setMap(x, y reflect.Value) {
+	x = indirect(x)
+	switch x.Kind() {
+	case reflect.Interface:
+		if x.NumMethod() != 0 {
+			s.panic("setMap", setError{src: y.Type(), dst: x.Type()})
+		}
+		x.Set(y)
+
+	case reflect.Map:
+		nm := reflect.MakeMap(x.Type())
+		for _, k := range y.MapKeys() {
+			nm.SetMapIndex(k, y.MapIndex(k))
+		}
+		x.Set(nm)
+
+	case reflect.Struct:
+		if y.Type().Key().Kind() != reflect.String {
+			s.panic("setMap", setError{src: y.Type(), dst: x.Type()})
+		}
+		fs := typeFields(x.Type())
+		for _, k := range y.MapKeys() {
+			f, ok := fs[k.String()]
+			if !ok {
+				continue
+			}
+			if f.readonly {
+				continue
+			}
+			s.set(x.Field(f.index), y.MapIndex(k))
+		}
+
+	default:
+		s.panic("setMap", setError{src: y.Type(), dst: x.Type()})
+	}
 }
 
-func (s *setState) setObject(x, y reflect.Value) {
+func (s setState) setStruct(x, y reflect.Value) {
+	x = indirect(x)
+	switch x.Kind() {
+	case reflect.Interface:
+		if x.NumMethod() != 0 {
+			s.panic("setStruct", setError{src: y.Type(), dst: x.Type()})
+		}
+		x.Set(y)
+
+	case reflect.Map:
+		if x.Type().Key().Kind() != reflect.String {
+			s.panic("setStruct", setError{src: y.Type(), dst: x.Type()})
+		}
+		nm := reflect.MakeMap(x.Type())
+		fs := typeFields(y.Type())
+		for _, f := range fs {
+			nm.SetMapIndex(reflect.ValueOf(f.name), y.Field(f.index))
+		}
+		x.Set(nm)
+
+	case reflect.Struct:
+		xfs := typeFields(x.Type())
+		yfs := typeFields(y.Type())
+		for k, yf := range yfs {
+			xf, ok := xfs[k]
+			if !ok {
+				continue
+			}
+			if xf.readonly {
+				continue
+			}
+			s.set(x.Field(xf.index), y.Field(yf.index))
+		}
+
+	default:
+		s.panic("setStruct", setError{src: y.Type(), dst: x.Type()})
+	}
+}
+
+func (s setState) panic(method string, err error) {
+	panic(errs.ErrorAt(method, err))
 }
--- framework/pkg/experimental/set_state_test.go
+++ framework/pkg/experimental/set_state_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/ironzhang/matrix/jsoncfg"
 )
@@ -78,6 +79,311 @@ func TestIndirectUnmarshaler(t *testing.T) {
 		}
 		if got, want := rv, tt.rv; got != want {
 			t.Errorf("tests[%d]: Value: got(%v) != want(%v)", i, got, want)
+		}
+	}
+}
+
+func TestSetValue(t *testing.T) {
+	if true {
+		casename := "SetHelloString"
+
+		var x string
+		var y string = "hello"
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, y; got != want {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetDurationString"
+
+		var x jsoncfg.Duration
+		var y string = "10m30s"
+		var w = jsoncfg.Duration(10*time.Minute + 30*time.Second)
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; got != want {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetArrayToArray"
+
+		var x = [5]int{5}
+		var y = [10]int{1, 2, 3}
+		var w = [5]int{1, 2, 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; got != want {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetArrayToSlice"
+
+		var x = []int{5, 4, 3, 2, 1}
+		var y = [3]int{1, 2, 3}
+		var w = []int{1, 2, 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetSliceToArray"
+
+		var x = [5]int{5, 4, 3, 2, 1}
+		var y = []int{1, 2, 3}
+		var w = [5]int{1, 2, 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; got != want {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetSliceToSlice"
+
+		var x = []int{5, 4, 3, 2, 1}
+		var y = []int{1, 2, 3}
+		var w = []int{1, 2, 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetSliceToInterface"
+
+		var x interface{}
+		var y = []int{1, 2, 3}
+		var w = []int{1, 2, 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetMapToInterface"
+
+		var x interface{}
+		var y = map[string]int{"1": 1, "2": 2}
+		var w = y
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetMapToMap"
+
+		var x map[string]int
+		var y = map[string]int{"1": 1, "2": 2}
+		var w = y
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetMapToStruct1"
+
+		type T struct {
+			A string
+			B int
+		}
+
+		var x T
+		var y = map[string]interface{}{"1": 1, "2": 2, "A": "A", "B": 3}
+		var w = T{A: "A", B: 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetMapToStruct2"
+
+		type T struct {
+			A map[string]int
+			B map[string]string
+			C map[int]int
+		}
+
+		var x T
+		var y = map[string]interface{}{
+			"A": map[string]int{
+				"A1": 1,
+				"A2": 2,
+			},
+			"B": map[string]string{
+				"B1": "1",
+				"B2": "2",
+			},
+			"C": map[int]int{
+				1: 1,
+				2: 2,
+			},
+		}
+		var w = T{
+			A: map[string]int{
+				"A1": 1,
+				"A2": 2,
+			},
+			B: map[string]string{
+				"B1": "1",
+				"B2": "2",
+			},
+			C: map[int]int{
+				1: 1,
+				2: 2,
+			},
+		}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetMapToStruct3"
+
+		type T struct {
+			A string
+			B int
+			C float64 `json:c`
+			D string  `json:",readonly"`
+			E string  `json:"-"`
+			f string
+		}
+
+		var x = T{D: "readonly", E: "nochange", f: "nochange"}
+		var y = map[string]interface{}{
+			"A": "A",
+			"B": 3,
+			"C": 4.1,
+			"D": "D",
+			"E": "E",
+			"f": "f",
+		}
+		var w = T{
+			A: "A",
+			B: 3,
+			C: 4.1,
+			D: "readonly",
+			E: "nochange",
+			f: "nochange",
+		}
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetStructToInterface"
+
+		type T struct {
+			A string
+			B int
+		}
+
+		var x interface{}
+		var y = T{A: "A", B: 3}
+		var w = T{A: "A", B: 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetStructToMap"
+
+		type T struct {
+			A string
+			B int
+		}
+
+		var x map[string]interface{}
+		var y = T{A: "A", B: 3}
+		var w = map[string]interface{}{"A": "A", "B": 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
+		}
+	}
+
+	if true {
+		casename := "SetStructToStruct"
+
+		type T1 struct {
+			A string
+			B int
+		}
+
+		type T2 struct {
+			X string `json:"A"`
+			Y int    `json:"B"`
+		}
+
+		var x T1
+		var y = T2{X: "A", Y: 3}
+		var w = T1{A: "A", B: 3}
+
+		if err := SetValue(&x, y); err != nil {
+			t.Fatalf("%s: %v", casename, err)
+		}
+		if got, want := x, w; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: %v != %v", casename, got, want)
 		}
 	}
 }
