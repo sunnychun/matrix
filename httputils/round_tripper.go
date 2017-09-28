ebfeb829a4a75df2435c7954d2d24c38e6b4d0d1
zhanghui <zhanghui@ablecloud.cn>
2017-09-28 12:26:38 +0800

    fix NopRoundTripper bug

--- httputils/round_tripper.go
+++ httputils/round_tripper.go
@@ -1,11 +1,23 @@
 package httputils
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
+
+type nopReadCloser struct {
+	io.Reader
+}
+
+func (p *nopReadCloser) Close() error {
+	return nil
+}
 
 type NopRoundTripper struct{}
 
 func (rt NopRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	return &http.Response{
 		StatusCode: http.StatusOK,
+		Body:       &nopReadCloser{r.Body},
 	}, nil
 }
